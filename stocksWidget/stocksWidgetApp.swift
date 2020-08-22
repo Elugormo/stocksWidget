5cd277b21735be5b59fedefb7264ea2c1f58086a
Johnny <romanomess@gmail.com>
2020-08-22 10:34:04 +0300

    Initial Commit

---
+++ stocksWidget/stocksWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  stocksWidgetApp.swift
+//  stocksWidget
+//
+//  Created by Johnny on 22.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct stocksWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
